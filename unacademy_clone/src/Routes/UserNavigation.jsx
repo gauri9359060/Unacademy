2ebfbba8f50020e5e83cd744dbc061142b4f597f
wizard3107 <ashishsingh9720@gmail.com>
2022-04-03 12:28:17 +0530

    initialzed and added the depedencies in the project

---
+++ unacademy_clone/src/Routes/UserNavigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+const UserNavigation = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserNavigation
