4fa62de8c3e6d74edad6bd5de1db75ade8d47c21
mjsvijit <viratsinghfzd75@gmail.com>
2022-04-06 20:32:43 +0530

    vijit changes

---
+++ unacademy_clone/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+<<<<<<< Updated upstream:unacademy_clone/src/App.js
+import Explore from './Components/explorepage/Explore';
+=======
+import Login from './Components/Login/Login';
+>>>>>>> Stashed changes:unacademy_clone/src/App.jsx
+
+
+
+function App() {
+  return (
+    <div className="App">
+<<<<<<< Updated upstream:unacademy_clone/src/App.js
+        <Explore/>
+=======
+     <Login />
+            
+>>>>>>> Stashed changes:unacademy_clone/src/App.jsx
+    </div>
+  );
+}
+
+export default App;
