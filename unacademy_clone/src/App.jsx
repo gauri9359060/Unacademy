b1d640c885ea414d06412d042e47bc2383310778
mjsvijit <viratsinghfzd75@gmail.com>
2022-04-07 00:45:26 +0530

    vijit login push 4

--- unacademy_clone/src/App.jsx
+++ unacademy_clone/src/App.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD:unacademy_clone/src/App.jsx
-=======
-
->>>>>>> 814a2daab782d050da8c56164086d6c263866db1:unacademy_clone/src/App.js
 import './App.css';
 import { Login } from './Components/Login/login';
 
