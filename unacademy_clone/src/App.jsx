400a92d2efc29cb06e34bea8418aab1de19ed993
mjsvijit <viratsinghfzd75@gmail.com>
2022-04-06 23:48:38 +0530

    vijit login page

--- unacademy_clone/src/App.jsx
+++ unacademy_clone/src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { Login } from './Components/Login/login';
 
 function App() {
   return (
     <div className="App">
       
+      <Login />
     </div>
   );
 }
