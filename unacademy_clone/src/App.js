9797889dc4b11c5f3943802918ab028bd2e914c6
Gauri Patil <72353440+gauri9359060@users.noreply.github.com>
2022-04-08 13:04:20 +0530

    added

--- unacademy_clone/src/App.js
+++ unacademy_clone/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-import Explore from './Components/explorepage/Explore';
+// import Explore from './Components/explorepage/Explore';
 import Footer from './Components/explorepage/Footer';
+import Home from './Components/HomeComponent/Home';
+import Navbar from './Components/Navbar/Navbar';
+
 
 
 
 function App() {
   return (
     <div className="App">
-        <Explore/>
+        <Navbar/>
+          <Home/>
         <Footer/>
     </div>
   );
--- unacademy_clone/src/Components/HomeComponent/Home.jsx
+++ unacademy_clone/src/Components/HomeComponent/Home.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from "styled-components"
+import styles from "./Home.module.css"
 
+const Container=styled.div`
+  width:100%
+`
+const Header = styled.div`
+  height: 500px;
+  background-repeat: no-repeat;
+  background-position: bottom right;
+  margin: auto;
+  max-width: 80%;
+  margin-bottom:50px;
+  padding-top: 0px;
+  padding-left: 30px;
+  width:100%;
+  background-image: url(https://static.uacdn.net/production/_next/static/images/home/hero_banner_landing_new.svg?q=75&w=3840);
+  @media (max-width: 768px) {
+    max-width:100%;
+  }
+  @media (max-width: 375px) {
+    margin-left:0px;
+    background:none;
+    position:sticky;
+  }
+
+`;
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Container>
+         <Header>
+              <div className={styles.headerh1}>
+                    <h1>India's largest <br/>learning platform</h1>
+                    <div>
+                    <Link to="/Explore">
+                      <button>ss</button>
+                    </Link>
+                     
+                    </div>
+              </div>
+
+         </Header>
+    </Container>
   )
 }
 
