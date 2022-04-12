c9d95b5ddffa85769bb16dc994daaf58232382a3
Gauri Patil <72353440+gauri9359060@users.noreply.github.com>
2022-04-12 21:10:20 +0530

    Update Cards.jsx

--- unacademy_clone/src/Components/HomeComponent/Cards.jsx
+++ unacademy_clone/src/Components/HomeComponent/Cards.jsx
@@ -8,7 +8,7 @@ const Cards = () => {
       }, []);
     
       const getData = () => {
-        fetch("http://localhost:3000/cards")
+        fetch("https://databaseunacademy.herokuapp.com/cards")
           .then((res) => res.json())
           .then((res) => setCard(res));
       };
